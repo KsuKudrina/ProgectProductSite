ab979273935569a56b5454598feebd3dc9141c03
Ksu <ksenia.barteva@gmail.com>
2024-05-26 00:17:38 +0700

    home work 6

---
+++ data.js
@@ -0,0 +1,47 @@
+const data = 
+`
+[
+    {
+        "img": "img/product/men/product_m1.jpg",
+        "title": "MANGO PEOPLE T-SHIRT",
+        "price": "$ 52.00"
+       
+    },
+    {
+        "img": "img/product/women/product_wm1.jpg",
+        "title": "MANGO PEOPLE T-SHIRT",
+        "price": "$ 52.00"
+    },
+    {
+        "img": "img/product/men/product_m2.jpg",
+        "title": "MANGO PEOPLE T-SHIRT",
+        "price": "$ 52.00"
+    },
+    {
+        "img": "img/product/women/product_wm2.jpg",
+        "title": "MANGO PEOPLE T-SHIRT",
+        "price": "$ 52.00"
+    },
+    {
+        "img": "img/product/women/product_wm3.jpg",
+        "title": "MANGO PEOPLE T-SHIRT",
+        "price": "$ 52.00"
+        
+    },
+    {
+        "img": "img/product/men/product_m3.jpg",
+        "title": "MANGO PEOPLE T-SHIRT",
+        "price": "$ 52.00"
+    },
+    {
+        "img": "img/product/men/product_m4.jpg",
+        "title": "MANGO PEOPLE T-SHIRT",
+        "price": "$ 52.00"
+    },
+    {
+        "img": "img/product/men/product_m5.jpg",
+        "title": "MANGO PEOPLE T-SHIRT",
+        "price": "$ 52.00"
+    }
+]
+`
